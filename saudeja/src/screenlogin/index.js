76142dd600e87ebdf662c9874edd446bd0d620a5
Mostind <joaopedroalmeida007@gmail.com>
2025-11-03 14:42:22 -0300

    Page Login front pronta

--- saudeja/src/screenlogin/index.js
+++ saudeja/src/screenlogin/index.js
@@ -1,14 +1,18 @@
 import React from "react";
-import {View , StyleSheet,TouchableOpacity,TextInput} from 'react-native';
+import {View , StyleSheet,TouchableOpacity,TextInput,Text} from 'react-native';
 import Banner from "./banner";
+
+
 
 const Screenlogin = () =>{
     return (
         <View style={styles.container}>
         <Banner/>
           <View style={styles.box_text}>
-            <TextInput style={styles.InputText} placeholder="CPF" />
+            <TextInput style={styles.InputText} placeholder="CPF" inputMode="numeric"/>
             <TextInput style={styles.InputText} placeholder="SENHA" />
+            <TouchableOpacity style={styles.button}><Text style={styles.ButtonText}>ACESSAR</Text></TouchableOpacity>
+            <Text style={styles.BaseboardText}>ESQUECI SENHA</Text>
           </View>
         </View>
     );
@@ -33,15 +37,35 @@ const styles=StyleSheet.create({
     backgroundColor:'#fff',
     marginBottom:50,
     padding:15,
-    width:257,
+    width:350,
     borderRadius:12,
     boxShadow:'0px 3px 1px #B3B3B3',
     // BORDA
     borderWidth:2,
     borderColor:'#B3B3B3',
-    borderStyle:'solid'
-    
-}
+    borderStyle:'solid',
+    color:'#272626ff',
+    fontSize:15,
+},
+ button:{
+    padding:10,
+    backgroundColor:'#009F80',
+    width:250,
+    height:54,
+    borderRadius:23
+ },
+ ButtonText:{
+    color:'#E8EFF1',
+    margin:'auto',
+ },
+ BaseboardText:{
+    marginTop:20,
+    color:'#00745cff',
+    fontWeight:'900',
+    fontSize:13
+ }
+
+
 });
 
 export default Screenlogin;
